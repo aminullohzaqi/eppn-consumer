40b79cbbb31db4b8973cb4033f83c853f1e4038c
Aminulloh Zaqi <aminulloh.zaqi@gmail.com>
2023-01-20 08:09:48 +0700

    modified:   src/DatabaseDWService.js 	modified:   src/whatsapp.js

--- src/DatabaseDWService.js
+++ src/DatabaseDWService.js
@@ -15,29 +15,23 @@ class DatabaseDWService {
 
     async getLastUpdate () {
         const query = {
-            text: 'SELECT * FROM core.etl_exec ORDER BY id DESC LIMIT 1',
+            text: `SELECT (SELECT tanggal_dan_waktu FROM core.etl_exec ORDER BY id DESC LIMIT 1) as tanggal_dan_waktu,
+            (SELECT tipe FROM core.etl_exec ORDER BY id DESC LIMIT 1) as tipe,
+            (SELECT status FROM core.etl_exec ORDER BY id DESC LIMIT 1) as status,
+            (SELECT COUNT(sk_pegawai) FROM core.fakta_mutasi) as row_fakta_mutasi,
+            (select COUNT(sk_pegawai) from core.fakta_dapat_disekolahkan) as row_fakta_dapat_disekolahkan,
+            (select COUNT(sk_pegawai) from core.fakta_hukuman) as row_fakta_hukuman,
+            (select COUNT(sk_pegawai) from core.fakta_ijin_belajar) as row_fakta_ijin_belajar,
+            (select COUNT(sk_fks) from core.fakta_kepala_satker) as row_fakta_kepala_satker,
+            (select COUNT(sk_pegawai) from core.fakta_naik_pangkat) as row_fakta_naik_pangkat,
+            (select COUNT(sk_pegawai) from core.fakta_penghargaan) as row_fakta_penghargaan,
+            (select COUNT(sk_pegawai) from core.fakta_series) as row_fakta_series`,
             values: []
         }
         const result = await this.pool.query(query)
         
         return result.rows
     }
-
-    // async getAdminServers () {
-    //     const result = await this.pool.query('SELECT server.displayname, admin.adminname, admin.email, admin.whatsapp FROM server INNER JOIN admin ON server.admin = admin.admin_id')
-
-    //     return result.rows
-    // }
-
-    // async getDetailServer (ip) {
-    //     const query = {
-    //         text: 'SELECT server.displayname, server.hostname, server.agentstatus, server.agentmessage, admin.adminname, admin.email, admin.whatsapp FROM server INNER JOIN admin ON server.admin = admin.admin_id WHERE server.hostname = $1',
-    //         values: [ip]
-    //     }
-    //     const result = await this.pool.query(query)
-
-    //     return result.rows
-    // }
 }
 
 module.exports = DatabaseDWService
