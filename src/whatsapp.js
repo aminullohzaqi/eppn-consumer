87f441b57d051b86cde06f905ee4be73c5f6608a
Aminulloh Zaqi <aminulloh.zaqi@gmail.com>
2023-01-25 10:22:21 +0700

    change model to ada

--- src/whatsapp.js
+++ src/whatsapp.js
@@ -136,7 +136,7 @@ function whatsappMessage() {
         else {
             try {
                 const completion = await openai.createCompletion({
-                    model: "text-davinci-003",
+                    model: "text-ada-001",
                     prompt: message.body,
                     temperature: 0.6,
                     max_tokens: 100
