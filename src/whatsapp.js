0f56ed8afb40cc50e493a4c20b91ca68c9001459
Aminulloh Zaqi <aminulloh.zaqi@gmail.com>
2023-02-10 11:34:16 +0700

    update model

--- src/whatsapp.js
+++ src/whatsapp.js
@@ -146,7 +146,7 @@ function whatsappMessage() {
         else {
             try {
                 const completion = await openai.createCompletion({
-                    model: "text-curie-001",
+                    model: "text-davinci-003",
                     prompt: message.body,
                     temperature: 0.6,
                     max_tokens: 250
